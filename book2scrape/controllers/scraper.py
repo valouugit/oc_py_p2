680cd8c0033113573f99477d119a4f316e6dcce3
valouugit <valentingoulier@gmail.com>
2022-04-15 16:41:42 +0200

    adding scraper for books in category

--- book2scrape/controllers/scraper.py
+++ book2scrape/controllers/scraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from ..models.category import Category
+from ..models.book import Book
 
 class Scraper:
     
@@ -22,3 +23,31 @@ class Scraper:
             ))
 
         return categories
+    
+    def getAllBookInCategory(self,
+                             category : Category,
+                             next : str = "",
+                             books : list = []) -> list[Book]:
+        res = requests.get("%s%s" % (category.url.rstrip("index.html"), next))
+        html = BeautifulSoup(res.content, "html.parser")
+        next = self.checkNextPage(html)
+        html = html.ol.find_all("article")
+        for book in html:
+            books.append(
+                Book(
+                    product_page_url=book\
+                        .a["href"]\
+                        .replace("../../..", "http://books.toscrape.com/catalogue")
+                        .rstrip("/index.html"),
+                    category=category.name
+                )
+            )
+        
+        if next != None:
+            self.getAllBookInCategory(category=category, next=next, books=books)
+        
+        return books
+    
+    def checkNextPage(self, html) -> str or None:
+        if html.find(class_="next") != None:
+            return html.find(class_="next").a["href"]
