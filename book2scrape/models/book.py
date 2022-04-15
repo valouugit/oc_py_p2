ee0b986f77e074ed81f093bc03c74661d18b2a9b
valouugit <valentingoulier@gmail.com>
2022-04-15 16:50:27 +0200

    url & category required

--- book2scrape/models/book.py
+++ book2scrape/models/book.py
@@ -3,13 +3,13 @@ from dataclasses import dataclass
 @dataclass
 class Book:
     """Class for book data storage"""
-    product_page_url: str = None
+    product_page_url: str
+    category: str
     universal_product_code: str = None
     title: str = None
     price_including_tax: float = None
     price_excluding_tax: float = None
     number_available: int = None
     product_description: str = None
-    category: str = None
     review_rating: float = None
     image_url: str = None
