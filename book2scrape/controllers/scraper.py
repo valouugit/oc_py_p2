cb8596c7b6cdf7941af1769a19324b0ce14d8b2a
valouugit <valentingoulier@gmail.com>
2022-04-15 14:48:38 +0200

    adding scraper & scrape all categories

---
+++ book2scrape/controllers/scraper.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+from ..models.category import Category
+
+class Scraper:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def getAllCategories(self) -> list[Category]:
+        res = requests.get("http://books.toscrape.com")
+        html = BeautifulSoup(res.content, "html.parser")
+        html = html.find_all("ul")[1]
+        categories = []
+        for li in html.find_all("li"):
+            categories.append(Category(
+                name=li.a.string.replace(" ", "").replace("\n", ""),
+                url="%s%s" % (
+                    "http://books.toscrape.com/",
+                    li.a["href"]
+                )
+            ))
+
+        return categories
