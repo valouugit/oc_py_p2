8b034d4e4387b2104b9763a22feb8324e2fe4133
valouugit <valentingoulier@gmail.com>
2022-04-26 14:03:11 +0200

    link list books in category

--- book2scrape/models/book.py
+++ book2scrape/models/book.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass
+from typing import Optional
 
 @dataclass
 class Book:
     """Class for book data storage"""
     product_page_url: str
     category: str
-    universal_product_code: str = None
-    title: str = None
-    price_including_tax: float = None
-    price_excluding_tax: float = None
-    number_available: int = None
-    product_description: str = None
-    review_rating: float = None
-    image_url: str = None
+    universal_product_code: Optional[str] = None
+    title: Optional[str] = None
+    price_including_tax: Optional[float] = None
+    price_excluding_tax: Optional[float] = None
+    number_available: Optional[int] = None
+    product_description: Optional[str] = None
+    review_rating: Optional[float] = None
+    image_url: Optional[str] = None
--- book2scrape/models/category.py
+++ book2scrape/models/category.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
+from typing import List
+from .book import Book
 
 @dataclass
 class Category:
     """Class for category data storage"""
     name: str
     url: str
+    books: List[Book]
