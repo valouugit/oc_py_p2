6e9df856b5dc962d62f1efec8e309a16beeb9e6a
valouugit <valentingoulier@gmail.com>
2022-04-26 14:04:02 +0200

    adapte new code & adding rich

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from book2scrape import Scraper, CsvWriter
+from rich.progress import Progress
 
-if __name__ == "__main__":
+def main():
     scraper = Scraper()
     csv = CsvWriter()
     
-    book = []
-    for cat in scraper.getAllCategories():
-        print("[Book in category] %s" % cat.name)
-        books = scraper.getAllBookInCategory(category=cat, books=[])
-        for book in books:
-            scraper.getBook(book)
-            print("[Book] Scraping %s" % book.title)
-            scraper.downloadBookImg(book=book)
-        print("[Save CSV] %s" % cat.name)
-        csv.writeBooks(books)
+    with Progress() as progress:
+        books_progress = progress.add_task("[yellow]Scraping books...", total=1000)
+        for cat in scraper.getAllCategories():
+            print("[Book in category] %s" % cat.name)
+            scraper.getAllBooksInCategory(category=cat)
+            for book in cat.books:
+                scraper.getBook(book)
+                progress.update(books_progress, advance=1)
+                print("[Book] Scraping %s" % book.title)
+                scraper.downloadBookImg(book=book)
+            print("[Save CSV] %s" % cat.name)
+            csv.writeBooks(cat)
+
+
+if __name__ == "__main__":
+    main()
